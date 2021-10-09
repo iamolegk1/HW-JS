d124fbc51af12447eac4a0fa82e4b3b8d90eb500
iamolegk1 <olegkropivka5@gmail.com>
2021-10-09 18:03:05 +0300

    Commit

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+// Задача №1 напиши функцию, которая принимает массив строк. С помощью метода map сформируй новый массив в котором будут только строки размером меньше 4 символов. Новый массив возвращай из функции.
+
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+function getNewMonth(data) {
+  const newMonth = data.map((month) => {
+    if (month.length <= 3) {
+      return month;
+    }
+  });
+  return newMonth;
+}
+console.log(getNewMonth(monthNames));
+
 // Задача №2 напиши функцию, которая принимает массив строк. С помощью метода forEach сформируй новый массив в котором будут только строки размером меньше 5 символов. Новый массив возвращай из функции.
 
 const monthNames = [
